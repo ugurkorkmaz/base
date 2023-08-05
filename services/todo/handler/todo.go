5237f333d69d6c42678ff61295b3bfec731633c3
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-08-05 11:56:46 +0300

    refactor: Update AddTask and UpdateTask parameters and types

--- services/todo/handler/todo.go
+++ services/todo/handler/todo.go
@@ -3,13 +3,13 @@ package handler
 import (
 	"context"
 	"log"
-	"time"
 
 	pb "app/grpc/todo"
 
-	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // Todo struct implements gRPC methods for the TodoService server
@@ -19,17 +19,15 @@ type TodoServer struct {
 }
 
 // AddTask is a gRPC method to add a task
-func (ts *TodoServer) AddTask(ctx context.Context, req *pb.Task) (*pb.Task, error) {
+func (ts *TodoServer) AddTask(ctx context.Context, req *pb.AddTaskRequest) (*pb.Task, error) {
 	// Create a new task and add it to the list
 	todo := &pb.Task{
-		Id:          req.Id,
+		Id:          uuid.NewString(),
 		Title:       req.Title,
 		Description: req.Description,
-		Completed:   req.Completed,
-		DueDate: &timestamp.Timestamp{
-			Seconds: time.Now().Unix(),
-			Nanos:   0,
-		},
+		Completed:   false,
+		CraeteDate:  timestamppb.Now(),
+		UpdateDate:  &timestamppb.Timestamp{},
 	}
 
 	ts.todos = append(ts.todos, todo)
@@ -37,7 +35,7 @@ func (ts *TodoServer) AddTask(ctx context.Context, req *pb.Task) (*pb.Task, erro
 	log.Printf("Adding task with title: %s\n", req.Title)
 
 	// Return the added task as it is for demonstration purposes
-	return req, nil
+	return todo, nil
 }
 
 // GetTask is a gRPC method to get a task
@@ -50,32 +48,31 @@ func (ts *TodoServer) GetTask(ctx context.Context, req *pb.GetTaskRequest) (*pb.
 				Title:       todo.Title,
 				Description: todo.Description,
 				Completed:   todo.Completed,
-				DueDate:     todo.DueDate,
+				CraeteDate:  todo.CraeteDate,
+				UpdateDate:  todo.UpdateDate,
 			}, nil
 		}
 	}
 
 	// Return an error if the task is not found
-
 	return nil, status.Errorf(codes.NotFound, "Task not found")
 }
 
 // UpdateTask is a gRPC method to update a task
-func (ts *TodoServer) UpdateTask(ctx context.Context, req *pb.Task) (*pb.Task, error) {
+func (ts *TodoServer) UpdateTask(ctx context.Context, req *pb.UpdateTaskRequest) (*pb.UpdateTaskResponse, error) {
 	// Update the task
 	for _, todo := range ts.todos {
 		if todo.Id == req.Id {
 			todo.Title = req.Title
 			todo.Description = req.Description
 			todo.Completed = req.Completed
-			todo.DueDate = &timestamp.Timestamp{
-				Seconds: time.Now().Unix(),
-				Nanos:   0,
-			}
+			todo.UpdateDate = timestamppb.Now()
 			log.Printf("Updating task with id: %s\n", req.Id)
 
 			// Return the updated task as it is for demonstration purposes
-			return req, nil
+			return &pb.UpdateTaskResponse{
+				Status: true,
+			}, nil
 		}
 	}
 
@@ -84,7 +81,7 @@ func (ts *TodoServer) UpdateTask(ctx context.Context, req *pb.Task) (*pb.Task, e
 }
 
 // DeleteTask is a gRPC method to delete a task
-func (ts *TodoServer) DeleteTask(ctx context.Context, req *pb.DeleteTaskRequest) (*pb.Task, error) {
+func (ts *TodoServer) DeleteTask(ctx context.Context, req *pb.DeleteTaskRequest) (*pb.DeleteTaskResponse, error) {
 	// Delete the task
 	for i, todo := range ts.todos {
 		if todo.Id == req.Id {
@@ -92,12 +89,8 @@ func (ts *TodoServer) DeleteTask(ctx context.Context, req *pb.DeleteTaskRequest)
 			log.Printf("Deleting task with id: %s\n", req.Id)
 
 			// Return the deleted task for demonstration purposes
-			return &pb.Task{
-				Id:          todo.Id,
-				Title:       todo.Title,
-				Description: todo.Description,
-				Completed:   todo.Completed,
-				DueDate:     todo.DueDate,
+			return &pb.DeleteTaskResponse{
+				Status: true,
 			}, nil
 		}
 	}
@@ -118,7 +111,8 @@ func (ts *TodoServer) ListTasks(ctx context.Context, req *pb.ListTasksRequest) (
 			Title:       todo.Title,
 			Description: todo.Description,
 			Completed:   todo.Completed,
-			DueDate:     todo.DueDate,
+			CraeteDate:  todo.CraeteDate,
+			UpdateDate:  todo.CraeteDate,
 		})
 	}
 
