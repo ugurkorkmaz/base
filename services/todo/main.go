8658c9b60fdabfd92445ad8768d2c9f63f2202f1
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-08-05 10:51:20 +0300

    refactor: add healt-check prop and update server address to :5051

--- services/todo/main.go
+++ services/todo/main.go
@@ -4,17 +4,20 @@ import (
 	"log"
 	"net"
 
-	todo "app/grpc/todo"
 	"app/handler"
 
 	"google.golang.org/grpc"
+
+	todo "app/grpc/todo"
+
+	health "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main() {
 	// Start and listen to the Golang gRPC server
 
-	// Specify the server address, for example, "localhost:50051"
-	address := "localhost:50051"
+	// Specify the server address, for example, "localhost:5051"
+	address := "localhost:5051"
 
 	// Create a network connection to listen on TCP
 	listener, err := net.Listen("tcp", address)
@@ -27,6 +30,9 @@ func main() {
 
 	// Register the TodoService server
 	todo.RegisterTodoServiceServer(server, &handler.TodoServer{})
+
+	// Register the HealthServer server
+	health.RegisterHealthServer(server, &handler.HealthServer{})
 
 	// Start the server and listen for client requests
 	log.Printf("Server listening on %s\n", address)
